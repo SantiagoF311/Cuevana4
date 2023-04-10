871514e83feb460ab684d95427c7a7e8c8ae537a
Santiago <santiagofonsecaf311@gmail.com>
2023-04-10 12:07:19 -0500

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,12 @@
-let apiKey = '35fb5b52b64fdf4959614d6ee6963661&language=en-US&page=';
-let currentPage = '1';
-const moviesPerPage = '20';
-const url =
-  `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}` + page;
-fetch(url)
+import {
+  API_KEY,
+  URL_POPULARES,
+  contenedorPeliculas,
+  mostrarPeliculas,
+  obtenerGeneros,
+} from './js/PelisPopulares.js';
+
+fetch(URL_POPULARES)
   .then(response => response.json())
-  .then(data => {
-    const movies = data.results;
-    const movieInfo = document.querySelector('#prueba');
-
-    movies.forEach(movie => {
-      const movieElement = document.createElement('div');
-      movieElement.innerHTML = `
-      <h2>${movie.title}</h2>`;
-
-      movieInfo.appendChild(movieElement);
-    });
-
-    const pagesButton = document.querySelector('#pages');
-    pagesButton.addEventListener('click', () => {
-      page++;
-      console.log(page);
-      console.log(url);
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  .then(data => mostrarPeliculas(data.results))
+  .catch(error => console.log(error));
